f87e814a17b6c9bfa310a3a9f05ab99b3630288d
Sourabh <sourabhdeshpande08@gmail.com>
2025-12-24 17:52:00 +0530

    First Commit

---
+++ SourabhProject/src/IT/Testers.java
@@ -0,0 +1,20 @@
+package IT;
+
+public class Testers {
+	
+	public void manual()
+	{
+		
+		System.out.println("This is manual tester");
+		
+	}
+	
+	
+	public void automation()
+	{
+		
+		System.out.println("This is automation tester");
+		
+	}
+
+}
---
+++ SourabhProject/src/automobiles/Mercedes.java
@@ -0,0 +1,23 @@
+package automobiles;
+
+public class Mercedes {
+	
+	public void colour()
+	{
+		
+		System.out.println("This is Mercedes colour");
+		
+		
+	}
+	
+	
+	public void model()
+	{
+		
+		System.out.println("This is Mercedes model");
+		
+		
+	}
+	
+
+}
