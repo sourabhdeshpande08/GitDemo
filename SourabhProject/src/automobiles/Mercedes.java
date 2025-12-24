e66d8ce9b32da15b4eddd031ebcb78ce106acdeb
Sourabh <sourabhdeshpande08@gmail.com>
2025-12-24 18:27:58 +0530

    This is third commit by US guy

--- SourabhProject/src/automobiles/Mercedes.java
+++ SourabhProject/src/automobiles/Mercedes.java
@@ -15,7 +15,8 @@ public class Mercedes {
 	{
 		
 		System.out.println("This is Mercedes model");
-		
+		System.out.println("This is Mercedes model 2");
+		System.out.println("This is Mercedes model 3");
 		
 	}
 	
