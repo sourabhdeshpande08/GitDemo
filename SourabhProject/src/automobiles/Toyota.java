5d6dd90af30a35767f81401cef0e06d4233affef
Sourabh <sourabhdeshpande08@gmail.com>
2025-12-24 19:36:39 +0530

    Architect American commit

---
+++ SourabhProject/src/automobiles/Toyota.java
@@ -0,0 +1,24 @@
+package automobiles;
+
+public class Toyota {
+	
+	public void colour()
+	{
+		
+		System.out.println("This is Toyota colour");
+		
+		
+		
+	}
+	
+	
+	public void model()
+	{
+		
+		System.out.println("This is Toyota model");
+		System.out.println("This is Toyota model 2");
+		System.out.println("This is Toyota model 3");
+	}
+	
+
+}
